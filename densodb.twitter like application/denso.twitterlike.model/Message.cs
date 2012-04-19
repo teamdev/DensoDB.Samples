98998f2fa7a1d84053a60c27b2d05a23c5504743
Paolo Possanzini <ppossanzini@gmail.com>
2012-04-19 18:54:30 +0200

    First commit of samples

---
+++ densodb.twitter like application/denso.twitterlike.model/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace denso.twitterlike.model
+{
+  public class Message
+  {
+    public DateTime Date { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Body { get; set; }
+  }
+}
